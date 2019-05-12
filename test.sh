1e8314c54e4489bf8da23fbf92c53dd33b46bf0a
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-05-12 15:26:02 +0430

    fix : test.sh updated

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
   set -e
   set -x
 
-  python -m vulture --min-confidence 80 --exclude=orangetool,build,.eggs --sort-by-size .
-  python -m bandit -r orangetool -s B607,B603,B404,B311
-  python -m pydocstyle --match-dir=orangetool
+  if [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
+  then
+	python -m vulture --min-confidence 80 --exclude=orangetool,build,.eggs --sort-by-size .
+	python -m bandit -r orangetool -s B607,B603,B404,B311
+	python -m pydocstyle --match-dir=orangetool
+  fi
