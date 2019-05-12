d7069498af404f4ba5aec4c555321822dca327d8
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-05-12 15:08:36 +0430

    fix : orangetool_ram docstrings modified

--- orangetool/orangetool_ram.py
+++ orangetool/orangetool_ram.py
@@ -3,7 +3,8 @@
 import psutil
 def convert_bytes(num):
     """
-    convert num to idiomatic byte unit
+    Convert num to idiomatic byte unit.
+
     :param num: the input number.
     :type num:int
     :return: str
@@ -19,55 +20,60 @@ def convert_bytes(num):
             return "%3.1f %s" % (num, x)
         num /= 1024.0
 def ram_total(convert=True):
-    '''
-    Return total ram of board
-    :param convert: Flag for convert mode (using of convert_byte function)
+    """
+    Return total ram of board.
+
+    :param convert: flag for convert mode (using of convert_byte function)
     :type convert:bool
     :return: total ram of board as string
-    '''
+    """
     response=list(psutil.virtual_memory())
     if convert==True:
         return convert_bytes(int(response[0]))
     else:
         return str(response[0])
 def ram_used(convert=True):
-    '''
-    Return how much ram is using
-    :param convert: Flag for convert mode (using of convert_byte function)
+    """
+    Return how much ram is using.
+
+    :param convert: flag for convert mode (using of convert_byte function)
     :type convert:bool
     :return: how much ram is using as string
-    '''
+    """
     response=list(psutil.virtual_memory())
     if convert == True:
         return convert_bytes(int(response[3]))
     else:
         return str(response[3])
 def ram_free(convert=True):
-    '''
-    Return how much ram is available
-    :param convert: Flag for convert mode (using of convert_byte function)
+    """
+    Return how much ram is available.
+
+    :param convert: flag for convert mode (using of convert_byte function)
     :type convert : bool
     :return: how much ram is available
-    '''
+    """
     response=list(psutil.virtual_memory())
     if convert == True:
         return convert_bytes(int(response[1]))
     else:
         return str(response[1])
 def ram_percent():
-    '''
-    Return available ram percentage
+    """
+    Return available ram percentage.
+
     :return: availabe ram percentage as string with %
-    '''
+    """
     response=list(psutil.virtual_memory())
     return str(response[2])+" %"
 def freeup(DEBUG=False):
-    '''
-    To free pagecache, dentries and inodes:
-    :param DEBUG: Flag for using Debug mode
+    """
+    To free pagecache, dentries and inodes.
+
+    :param DEBUG: flag for using Debug mode
     :type DEBUG:bool
     :return: Amount of freeuped ram as string and converted by convert_bytes()
-    '''
+    """
     try:
         RAM_before=int(ram_free(convert=False))
         caches_control=open("/proc/sys/vm/drop_caches","w")
