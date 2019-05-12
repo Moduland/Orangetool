de423a153866f31595199aa9a6a8b06320dd9573
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-05-12 15:03:13 +0430

    fix : orangetool_display docstrings modified

--- orangetool/orangetool_display.py
+++ orangetool/orangetool_display.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """Orangetool display functions."""
 def hdmi_on(DEBUG=False):
-    '''
-    This function turn on hdmi port (need sudo -s)
-    :param DEBUG: Flag for using Debug mode
+    """
+    Turn on hdmi port (need sudo -s).
+
+    :param DEBUG: flag for using Debug mode
     :type DEBUG:bool
     :return: bool
-    '''
+    """
     try:
         hdmi_control=open("/sys/class/graphics/fb0/blank","w")
         hdmi_control.write("0")
@@ -18,12 +19,13 @@ def hdmi_on(DEBUG=False):
         return "Error"
 
 def hdmi_off(DEBUG=False):
-    '''
-       This function turn off hdmi port (need sudo -s)
-       :param DEBUG: Flag for using Debug mode
-       :type DEBUG:bool
-       :return: bool
-       '''
+    """
+    Turn off hdmi port (need sudo -s).
+
+    :param DEBUG: flag for using Debug mode
+    :type DEBUG:bool
+    :return: bool
+    """
     try:
         hdmi_control=open("/sys/class/graphics/fb0/blank","w")
         hdmi_control.write("4")
@@ -34,16 +36,17 @@ def hdmi_off(DEBUG=False):
             print(str(e))
         return "Error"
 def hdmi_size(v=None,h=None,DEBUG=False):
-    '''
-    This function change hdmi display resolution (need sudo -s) (if call without any argument return current resolution)
+    """
+    Change hdmi display resolution (need sudo -s) (if call without any argument return current resolution).
+
     :param v: vertical line
     :param h: horizental line
-    :param DEBUG: Flag for using Debug mode
+    :param DEBUG: flag for using Debug mode
     :type v : int
     :type h:int
     :type DEBUG:bool
     :return: bool
-    '''
+    """
     try:
         if (not isinstance(v,int)) or (not isinstance(h,int)):
             hdmi_control = open("/sys/class/graphics/fb0/virtual_size", "r")
