526b074060b2ea537182d66b25a5da9e90468225
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-11-30 00:48:45 +0330

    fix : minor edit in vulture test call

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
   python version_check.py
   if [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
   then
-	python -m vulture --min-confidence 80 --exclude=orangetool,build,.eggs --sort-by-size .
+	python -m vulture --min-confidence 60 --sort-by-size orangetool setup.py version_check.py
 	python -m bandit -r orangetool -s B607,B603,B404,B311
 	python -m pydocstyle --match-dir=orangetool
   fi
