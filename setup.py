9bd6c7646903b5d9838b61f0dbd41a3ddd4ae5c0
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-11-17 01:27:22 +0330

    fix : minor edit in setup.py

--- setup.py
+++ setup.py
@@ -48,6 +48,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering',
     ],
     install_requires=get_requires(),
