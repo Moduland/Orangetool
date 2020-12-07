d4fd9b76475b13ceb4939cd887520b37b28b0a02
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-12-07 18:58:08 +0330

    fix : minor edit in test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
   python version_check.py
   if [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
   then
-	python -m vulture --min-confidence 65 --sort-by-size orangetool setup.py version_check.py
+	python -m vulture orangetool/ setup.py version_check.py --min-confidence 65 --sort-by-size 
 	python -m bandit -r orangetool -s B607,B603,B404,B311
-	python -m pydocstyle --match-dir=orangetool
+	python -m pydocstyle -v --match-dir=orangetool
   fi
