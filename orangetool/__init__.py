19c1421f26efac323a1e4f8e66f3b5d7b9dfda91
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-12-07 22:51:33 +0330

    fix : __init__ file updated

--- orangetool/__init__.py
+++ orangetool/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """Orangetool modules."""
 
-from .orangetool_display import *
-from .orangetool_ip import *
-from .orangetool_system import *
-from .orangetool_ram import *
-from .orangetool_storage import *
+from .orangetool_display import hdmi_on, hdmi_off, hdmi_size
+from .orangetool_ip import internet, local_ip, global_ip, set_ip, ping, mac
+from .orangetool_system import check_update, get_temp, uptime, idletime, wakeup, version, sleep, hibernate, halt, restart
+from .orangetool_ram import ram_total, ram_used, ram_free, ram_percent, freeup
+from .orangetool_storage import mount_status, storage_status, unmount, unmount_all, mount, usb_on, usb_off
 from .orangetool_params import ORANGETOOL_VERSION
 
 __version__ = ORANGETOOL_VERSION
