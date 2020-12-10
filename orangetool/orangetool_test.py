00290857ec2970c6aaf319b12ee6f780e09c632a
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-12-10 14:05:59 +0330

    fix : minor edit in orangetool_test

--- orangetool/orangetool_test.py
+++ orangetool/orangetool_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Orangetool test."""
-from orangetool import *
+from orangetool import version, check_update
 
 if __name__ == "__main__":
     version()
+    check_update()
